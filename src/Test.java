6043311c5b06ecca1cb8317a7fb9508e9088a49a
Jackupup <zhanggaoqi0403@163.com>
2021-01-06 09:20:21 +0800

    666

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+/**
+ * @Package: PACKAGE_NAME
+ * @ClassName: Test
+ * @Author: HelloSon
+ * @CreateTime: 2021/1/6 9:19
+ * @Description:
+ */
+public class Test
+{
+    public static void main(String[] args) {
+        System.out.println("6655555");
+        System.out.println("6655555");
+        System.out.println("6655555");
+    }
+}
